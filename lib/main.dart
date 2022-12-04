f53c06eeb425ca086390d262d98ad816fb3cd81e
Abdullah DÜNDAR <64316056+abdullahD00@users.noreply.github.com>
2022-12-04 20:56:34 +0300

    spashscreen view draft create

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipe/view/authenticate/splash/view/splash_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,24 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: const Center(),
+      home: const SplashScreenView(),
     );
   }
 }
--- lib/view/authenticate/splash/view/splash_view.dart
+++ lib/view/authenticate/splash/view/splash_view.dart
@@ -1,11 +1,31 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashScreenView extends StatelessWidget {
   const SplashScreenView({super.key});
-
+  final String lottieUrlThinking = "https://assets4.lottiefiles.com/packages/lf20_ysas4vcp.json";
+  final String lottieUrlLoading = "https://assets10.lottiefiles.com/private_files/lf30_cjoryulu.json";
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            Lottie.network(lottieUrlThinking),
+            Text(
+              "RECIPE",
+              style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                    fontSize: 60,
+                    fontWeight: FontWeight.w300,
+                    color: Colors.pink,
+                  ),
+            ),
+            SizedBox(height: 250, child: Lottie.network(lottieUrlLoading)),
+          ],
+        ),
+      ),
+    );
   }
 }
